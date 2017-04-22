7eedd4131c7ed2d48fb8a16bc65202c2f6f0b376
Sean Matthews <matthews.sean.d@gmail.com>
2017-04-22 20:15:35 +0000

    Adding in some express so we can run this on a server

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 var tmi = require('tmi.js');
 var options = require('./config.json');
 
+var express = require('express')
+var app = express()
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.engine('html', require('ejs').renderFile);
+
+
+app.get('/', function (req, res) {
+  res.render('index.html')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
+
 var client = new tmi.client(options);
 
 client.on("chat", function (channel, userstate, message, self) {
