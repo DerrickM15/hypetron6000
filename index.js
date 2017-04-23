3c76c73ac8bec7ce184c9c22861b57b8bb410b76
Sean Matthews <matthews.sean.d@gmail.com>
2017-04-23 08:56:32 -0500

    Making it read the actual number of emotes instead of the number of unique emotes

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-var tmi = require('tmi.js');
-var options = require('./config.json');
+const tmi = require('tmi.js');
+const express = require('express');
+const options = require('./config.json');
 
-var express = require('express');
-var app = express();
+const app = express();
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static('Static'));
 
-
-
 app.get('/', function (req, res) {
   res.render('index.html');
-})
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
-})
+});
 
 
-var client = new tmi.client(options);
+const client = new tmi.client(options);
 
 client.on("chat", function (channel, userstate, message, self) {
     // Don't listen to my own messages..
     if (self) return;
-    
-    var emotesNum = userstate['emotes'] ? Object.keys(userstate['emotes']).length : 0;
-    console.log(emotesNum);
+
+    if (userstate['emotes']) {
+        let emotesNum = 0;
+        USEmotesObj = userstate['emotes'];
+        for (let key in USEmotesObj) {
+            if (USEmotesObj.hasOwnProperty(key)) {
+                emotesNum += USEmotesObj[key].length;
+            }
+            console.log(emotesNum);
+        }
+    }
 });
 
 // Connect the client to the server..
