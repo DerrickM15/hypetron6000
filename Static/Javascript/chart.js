cd77c7638cc48a9cbbbfbb0a8df9e28a1fe529cc
DerrickM15 <marzend88@gmail.com>
2017-04-23 10:36:41 -0500

    HYPETRON IS ALIVE

--- Static/Javascript/chart.js
+++ Static/Javascript/chart.js
@@ -1,12 +1,30 @@
    google.charts.load('current', {'packages':['gauge']});
       google.charts.setOnLoadCallback(drawChart);
 
+
       function drawChart() {
-        var hype = 50 //pull information from websocket magic
+        var hype = 0 //pull information from websocket magic
         var data = google.visualization.arrayToDataTable([
           ['Label', 'Value'],
           ['Hypetron', hype],//0 === starting value
         ]);
+
+          skateboard('http://localhost:8080/skateboard?param=true', function(stream) {
+          stream.pipe(stream);
+          stream.on('data', function(d) {
+          hype = hype + parseInt(d);
+          console.log(hype);
+          });
+          stream.on('disconnection', function() {
+              console.log('disconnected')
+          });
+          stream.on('reconnection', function() {
+              console.log('reconnected')
+          })
+      }).on('connection', function() {
+          console.log('connected')
+      });
+
 
         var options = {
           width: 320, height: 320,
@@ -24,7 +42,10 @@
           hype = hype - 1;
           if (hype < 1) {
             hype = 0;
-          }       
+          }
+          else if (hype > 105) {
+            hype = 100;
+          }      
           data.setValue(0, 1, hype);//60 * Math.round replaced with emotesNum /2?
           chart.draw(data, options);
         }, 1000);//update interval
